2cd049865062985937481659a201421a33b6eac0
davitshirkhanyan <davit.shirkhanyan@gmail.com>
2020-12-26 20:32:31 -0800

    add null response handling

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,11 +4,12 @@ var startGame = function() {
     playerInfo.reset();
 
 // run fight function to start game
-for(let i = 0; i < enemyInfo.lenght; i++) {
+for(let i = 0; i < enemyInfo.length; i++) {
     // if player is still alive, keep fighting
     if (playerInfo.health > 0) {
         // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        
         // pick new enemy to fight based on the index of the enemy.names array
         var pickedEnemyObj = enemyInfo[i];
 
@@ -162,8 +163,18 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+    while (name === "" || name === null) {
+        name = prompt("What's your robot's name?");
+    }
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
